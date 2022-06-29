f8f271c483bb87c8e4bf8923ef084b8b6079fb1c
James Beta <jamesbeta99@outlook.com>
2022-06-29 16:55:34 +0300

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -42,26 +42,6 @@ import pandas as pd
 from PIL import Image
 import base64
 
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-        <style>
-        body {
-        background-image: url("data:image/png;base64,%s");
-        background-size: cover;
-        }
-        </style>'''
-         % bin_str
-
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
 
 # Vectorizer
 news_vectorizer = open("resources/tfidf.pkl","rb")
@@ -80,7 +60,6 @@ def main():
     # these are static across all pages
     st.title("Tweet Classifer")
     st.subheader("Climate change tweet classification")
-    set_png_as_page_bg('background.png')
     # Creating sidebar with selection box you can create multiple pages this way
     options = ["Prediction", "Information"]
     selection = st.sidebar.selectbox("Choose Option", options)
